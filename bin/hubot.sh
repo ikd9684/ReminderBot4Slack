38b7e23dadce6a70f41372de96303c90e09efebd
Shigehiro Ikeda <ikd9684@gmail.com>
2016-02-20 21:39:50 +0900

    personal access token を外部ファイル化。

--- bin/hubot.sh
+++ bin/hubot.sh
@@ -4,6 +4,7 @@ set -e
 
 npm install
 export PATH="node_modules/.bin:node_modules/hubot/node_modules/.bin:$PATH"
-export HUBOT_SLACK_TOKEN=xoxb-22321469687-7wkNHMSwBaFdF7Nb3QpYhcUy
+export HUBOT_SLACK_TOKEN=`cat .hubot_slack_token`
 
 exec node_modules/.bin/hubot --name "reminder" "$@"
+
